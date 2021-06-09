3228133f1b7eb858375ba2d4f20b4ec2ce437ebe
Quintine <quintine_eier@hotmail.com>
2021-06-09 18:01:22 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=34911b3b1f335cf458517358657aaf10&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback("Unable to connect to weather services", undefined)
+        } else if (body.error) {
+            callback("Unable to find location, please try again.", undefined)
+        } else {
+            const temperature = body.current.temperature
+            const feelslike = body.current.feelslike
+            const weatherDescription = body.current.weather_descriptions[0]
+            callback(undefined, weatherDescription + ". It is currently " + temperature + " degrees Celcius outside, but it feels like " + feelslike + " degrees.")
+        }
+    })
+}
+
+module.exports = forecast
+
---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoieHF1aW50byIsImEiOiJja3BqdXhvN2wxcGJjMndyaXQwOWFkc2VrIn0.wKAV-TkkC9_CHN9Suj7tZg&limit=1'
+
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Please try again!', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+
+module.exports = geocode
+    
+
