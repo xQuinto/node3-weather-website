99b04d32933e4f6a085f9fabb824f66d59f67552
Quintine <quintine_eier@hotmail.com>
2021-06-13 12:34:00 +0200

    Updated with tabel and use openweather

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,22 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
+const day1Date = document.querySelector('#day1-date')
+const day1Min = document.querySelector('#day1-min')
+const day1Max = document.querySelector('#day1-max')
+const day1Description = document.querySelector('#day1-description')
+//const day1Image = document.querySelector('#day1-image')
+
+const day2Date = document.querySelector('#day2-date')
+const day2Min = document.querySelector('#day2-min')
+const day2Max = document.querySelector('#day2-max')
+const day2Description = document.querySelector('#day2-description')
+
+const day3Date = document.querySelector('#day3-date')
+const day3Min = document.querySelector('#day3-min')
+const day3Max = document.querySelector('#day3-max')
+const day3Description = document.querySelector('#day3-description')
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -15,8 +31,27 @@ weatherForm.addEventListener('submit', (e) => {
             if (data.error) {
                 messageOne.textContent = data.error
             } else {
-                messageOne.textContent = data.forecast
+                messageOne.textContent = "It is currently " + data.forecast.temperature + " degrees, but it feels like " + data.forecast.feelslike + " degrees."
                 messageTwo.textContent = data.location
+
+                day1Date.textContent = data.forecast.string_today_date
+                day1Min.textContent = data.forecast.today_min
+                day1Max.textContent = data.forecast.today_max
+                day1Description.textContent = data.forecast.today_description
+                document.getElementById('day1-image').src = "https://openweathermap.org/img/wn/" + data.forecast.today_image + "@2x.png"
+                //day1Image.innerHTML = "https://openweathermap.org/img/wn/01d@2x.png"
+
+                day2Date.textContent = data.forecast.string_tomorrow_date
+                day2Min.textContent = data.forecast.tomorrow_min
+                day2Max.textContent = data.forecast.tomorrow_max
+                day2Description.textContent = data.forecast.tomorrow_description
+                document.getElementById('day2-image').src = "https://openweathermap.org/img/wn/" + data.forecast.tomorrow_image + "@2x.png"
+
+                day3Date.textContent = data.forecast.string_day3_date
+                day3Min.textContent = data.forecast.day3_min
+                day3Max.textContent = data.forecast.day3_max
+                day3Description.textContent = data.forecast.day3_description
+                document.getElementById('day3-image').src = "https://openweathermap.org/img/wn/" + data.forecast.day3_image + "@2x.png"
             }
         })    
 })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=34911b3b1f335cf458517358657aaf10&query=' + latitude + ',' + longitude + '&units=m'
+    //const url = 'http://api.weatherstack.com/current?access_key=34911b3b1f335cf458517358657aaf10&query=' + latitude + ',' + longitude + '&units=m'
+    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=534f23b68fb8dc08545c7c524833ac7a'
 
     request({ url, json: true}, (error, { body }) => {
         if (error) {
@@ -9,13 +10,46 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("Unable to find location, please try again.", undefined)
         } else {
-            const temperature = body.current.temperature
-            const feelslike = body.current.feelslike
-            const weatherDescription = body.current.weather_descriptions[0]
-            const humidity = body.current.humidity
-            const uv_index = body.current.uv_index
-            callback(undefined, weatherDescription + ". It is currently " + temperature + " degrees Celcius outside, but it feels like " + feelslike + " degrees.\nThe humidity is " + humidity + "%.\nThe uv index is: " + uv_index + ".")
+            const temperature = body.current.temp
+            const feelslike = body.current.feels_like
+
+            // De datums berekenen
+            const today_date_unix = body.daily[0].dt * 1000
+            const today_date = new Date(today_date_unix)
+            const string_today_date = today_date.getDate() + "/" + (today_date.getMonth()+1)
+
+            const tomorrow_date_unix = body.daily[1].dt * 1000
+            const tomorrow_date = new Date(tomorrow_date_unix)
+            const string_tomorrow_date = tomorrow_date.getDate() + "/" + (tomorrow_date.getMonth()+1)
+
+            const day3_date_unix = body.daily[2].dt * 1000
+            const day3_date = new Date(day3_date_unix)
+            const string_day3_date = day3_date.getDate() + "/" + (day3_date.getMonth()+1)
+
+            // De overige data uit api halen
+            const today_min = body.daily[0].temp.min
+            const today_max = body.daily[0].temp.max
+            const today_description = body.daily[0].weather[0].description
+            const today_image = body.daily[0].weather[0].icon
+
+            const tomorrow_min = body.daily[1].temp.min
+            const tomorrow_max = body.daily[1].temp.max
+            const tomorrow_description = body.daily[1].weather[0].description
+            const tomorrow_image = body.daily[1].weather[0].icon
+
+            const day3_min = body.daily[2].temp.min
+            const day3_max = body.daily[2].temp.max
+            const day3_description = body.daily[2].weather[0].description
+            const day3_image = body.daily[2].weather[0].icon
+            
+            
+            // De variabelen sturen naar /weather
+            callback(undefined, {temperature, feelslike, 
+                string_today_date, today_min, today_max, today_description, today_image, 
+                string_tomorrow_date, tomorrow_min, tomorrow_max, tomorrow_description, tomorrow_image, 
+                string_day3_date, day3_min, day3_max, day3_description, day3_image})
         }
+        
     })
 }
 
